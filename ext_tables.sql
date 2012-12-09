e5c6e313ea475b283c93604c55028653bfcf9ea5
Matthias Nitsch <matthias.nitsch@me.com>
2012-12-09 14:14:38 +0100

    [TASK] Finalized multilanguage capability

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,3 +6,12 @@ CREATE TABLE pages (
 	tx_facebookopengraph_admins tinytext NOT NULL,
 	tx_facebookopengraph_description text NOT NULL
 );
+
+CREATE TABLE pages_language_overlay (
+	tx_facebookopengraph_title text NOT NULL,
+	tx_facebookopengraph_type text NOT NULL,
+	tx_facebookopengraph_image tinytext NOT NULL,
+	tx_facebookopengraph_site_name tinytext NOT NULL,
+	tx_facebookopengraph_admins tinytext NOT NULL,
+	tx_facebookopengraph_description text NOT NULL
+);
