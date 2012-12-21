a29c62ea21698028864075f214594e3058e1781a
Matthias Nitsch <matthias.nitsch@me.com>
2012-12-21 14:30:00 +0100

    [RELEASE] Release of version 1.2.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,15 @@
 <?php
 
+/***************************************************************
+ * Extension Manager/Repository config file for ext "facebook_opengraph".
+ *
+ * Auto generated 21-12-2012 14:26
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Facebook Open Graph',
 	'description' => 'Extension for adding Open Graph meta tags to pages.',
@@ -12,21 +22,22 @@ $EM_CONF[$_EXTKEY] = array(
 	'module' => '',
 	'state' => 'beta',
 	'internal' => '',
-	'uploadfolder' => '1',
+	'uploadfolder' => 1,
 	'createDirs' => '',
 	'modify_tables' => 'pages, pages_language_overlay',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.2.1',
+	'version' => '1.2.2',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.7',
+			'typo3' => '4.7-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
+	'_md5_values_when_last_written' => 'a:12:{s:12:"ext_icon.gif";s:4:"5342";s:17:"ext_localconf.php";s:4:"cba2";s:14:"ext_tables.php";s:4:"1cdb";s:14:"ext_tables.sql";s:4:"593f";s:9:"README.md";s:4:"2232";s:34:"Configuration/TypoScript/setup.txt";s:4:"7459";s:37:"Documentation/_IncludedDirectives.rst";s:4:"cc43";s:23:"Documentation/Index.rst";s:4:"5ea9";s:36:"Documentation/ProjectInformation.rst";s:4:"4809";s:28:"Documentation/UserManual.rst";s:4:"7f9d";s:44:"Resources/Private/Language/locallang_csh.xlf";s:4:"d41d";s:43:"Resources/Private/Language/locallang_db.xlf";s:4:"4b83";}',
 );
 
 ?>
