3545d0e68439781eb6ebd78ebf92dc93f408d7c3
Matthias Nitsch <matthias.nitsch@me.com>
2012-12-26 12:39:51 +0100

    [RELEASE] Release of version 1.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "facebook_opengraph".
  *
- * Auto generated 21-12-2012 14:26
+ * Auto generated 26-12-2012 12:39
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => 'pages, pages_language_overlay',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.2.2',
+	'version' => '1.3.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.7-0.0.0',
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:12:"ext_icon.gif";s:4:"5342";s:17:"ext_localconf.php";s:4:"cba2";s:14:"ext_tables.php";s:4:"1cdb";s:14:"ext_tables.sql";s:4:"593f";s:9:"README.md";s:4:"2232";s:34:"Configuration/TypoScript/setup.txt";s:4:"7459";s:37:"Documentation/_IncludedDirectives.rst";s:4:"cc43";s:23:"Documentation/Index.rst";s:4:"5ea9";s:36:"Documentation/ProjectInformation.rst";s:4:"4809";s:28:"Documentation/UserManual.rst";s:4:"7f9d";s:44:"Resources/Private/Language/locallang_csh.xlf";s:4:"d41d";s:43:"Resources/Private/Language/locallang_db.xlf";s:4:"4b83";}',
+	'_md5_values_when_last_written' => 'a:17:{s:12:"ext_icon.gif";s:4:"5342";s:17:"ext_localconf.php";s:4:"b722";s:14:"ext_tables.php";s:4:"1cdb";s:14:"ext_tables.sql";s:4:"8c60";s:9:"README.md";s:4:"4781";s:38:"Configuration/TypoScript/constants.txt";s:4:"cd68";s:34:"Configuration/TypoScript/setup.txt";s:4:"0e05";s:37:"Documentation/_IncludedDirectives.rst";s:4:"301f";s:37:"Documentation/AdministratorManual.rst";s:4:"0e45";s:23:"Documentation/Index.rst";s:4:"8dfb";s:36:"Documentation/ProjectInformation.rst";s:4:"beb1";s:37:"Documentation/TyposcriptReference.rst";s:4:"d1ef";s:28:"Documentation/UserManual.rst";s:4:"5434";s:43:"Documentation/Images/screen_page_module.jpg";s:4:"e6bb";s:44:"Resources/Private/Language/locallang_csh.xlf";s:4:"d41d";s:43:"Resources/Private/Language/locallang_db.xlf";s:4:"4b83";s:41:"Resources/Public/Images/default_image.png";s:4:"c139";}',
 );
 
 ?>
